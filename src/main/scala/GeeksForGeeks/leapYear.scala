d5bf98b41fb94a51e74dd291115209e9058a7150
Ajay Kumar <ajay.spark3765@gmail.com>
2024-06-02 16:17:46 +0530

    Git Bash Commit : Finding the maximum value from a List

--- src/main/scala/GeeksForGeeks/leapYear.scala
+++ src/main/scala/GeeksForGeeks/leapYear.scala
@@ -2,10 +2,10 @@ package GeeksForGeeks
 import scala.io.StdIn.readInt
 object leapYear {
   def main(args:Array[String]):Unit={
-    val a = Array(1,4,7,8,3,5,9,2)
+    val a = List(1,4,7,8,3,5,9,2)
     println(findMax(a))
 
-    def findMax(k:Array[Int]):Int={
+    def findMax(k:List[Int]):Int={
       val b = k.max
       return b
     }
