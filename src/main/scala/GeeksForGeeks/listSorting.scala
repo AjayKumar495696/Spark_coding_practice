95d7b4fbe699edcfb676c239dc05d6856364eb8b
Ajay Kumar <ajay.spark3765@gmail.com>
2024-06-03 15:37:16 +0530

    Now printing distinct list from the given list

--- src/main/scala/GeeksForGeeks/listSorting.scala
+++ src/main/scala/GeeksForGeeks/listSorting.scala
@@ -4,8 +4,10 @@ object listSorting {
   def main(args:Array[String]):Unit={
     val list:List[Int] = List(2,1,6,2,3,7,4,7,9,5,8)
     val sortedList:List[Int] = list.sorted
+    val listDistinct:List[Int] = list.distinct
     println(list)
     println(sortedList)
+    println(listDistinct)
   }
 
 }
